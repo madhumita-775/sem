2058d9941ed664bb8dadbdd2acc3bf8243dac313
madhumita-775 <madhudevil390@gmail.com>
2024-10-09 10:40:04 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,9 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) { // Check if the number is odd
+                System.out.println(i);
+            }
+        }
+    }
+}
